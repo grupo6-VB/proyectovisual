ef407f4b781c4d614bd49c308cad03abf38a8ce7
Hector-Luis <hluischimbo@gmail.com>
2016-11-30 20:55:14 -0500

    recinto y partidos

---
+++ ProyectoVB-grupo6/ProyectoVB-grupo6/Partido_Politico.vb
@@ -0,0 +1,41 @@
+﻿Public Class Partido_Politico
+    Private _id As String
+    Public Property Id() As String
+        Get
+            Return _id
+        End Get
+        Set(ByVal value As String)
+            _id = value
+        End Set
+    End Property
+
+    Private _nombre As String
+    Public Property Nombre() As String
+        Get
+            Return _nombre
+        End Get
+        Set(ByVal value As String)
+            _nombre = value
+        End Set
+    End Property
+
+    Private _estado As Boolean
+    Public Property Estado() As Boolean
+        Get
+            Return _estado
+        End Get
+        Set(ByVal value As Boolean)
+            _estado = value
+        End Set
+    End Property
+
+    Private _candidatos As ArrayList
+    Public Property Candidatos() As ArrayList
+        Get
+            Return _candidatos
+        End Get
+        Set(ByVal value As ArrayList)
+            _candidatos = value
+        End Set
+    End Property
+End Class
--- ProyectoVB-grupo6/ProyectoVB-grupo6/Votos.vb
+++ ProyectoVB-grupo6/ProyectoVB-grupo6/Votos.vb
@@ -1,5 +1,13 @@
 ﻿Public Class Votos
 
-    Property votantes As ArrayList
+    Private _votantes As ArrayList
+    Public Property Candidatos() As ArrayList
+        Get
+            Return _votantes
+        End Get
+        Set(ByVal value As ArrayList)
+            _votantes = value
+        End Set
+    End Property
 
 End Class
